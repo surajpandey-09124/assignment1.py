9468f47af040b18e3421362b388c8fb74a40cd93
surajpandey-09124 <surajpandey09124@gmail.com>
2025-09-26 14:57:41 +0530

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,4 @@
+first_name=input('enter your first name :')
+last_name=input('enter your last name :')
+full_name=first_name+" "+last_name
+print(f'hello,{full_name}! welcome to the python program')
