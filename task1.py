8f8fe5a349f3bf2d71ed5e98f8f6abdd35a011f5
surajpandey-09124 <surajpandey09124@gmail.com>
2025-09-26 14:56:19 +0530

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,12 @@
+x=input('enter the number :')
+x=int(x)
+y=input('enter the number :')
+y=int(y)
+addition=x+y
+subtraction=x-y
+multiplication=x*y
+division=x/y
+print('addition :',addition)
+print('subtraction :',subtraction)
+print('multiplication :',multiplication)
+print('division :',division)
